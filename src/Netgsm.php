d224bc732ee8cccd5dc4a3ebc29d8a435e0a217a
Burak <destek@burakakdeniz.com.tr>
2017-08-24 17:03:37 +0300

    changes

---
+++ src/Netgsm.php
@@ -0,0 +1,10 @@
+<?php namespace Brkphp\Netgsm;
+
+class Netgsm {
+
+    public static function saySomething() {
+       return trans("netgsm::sms.error");
+    }
+
+
+}
---
+++ src/NetgsmServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Brkphp\Netgsm;
+
+use Illuminate\Support\ServiceProvider;
+
+class NetgsmServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadTranslationsFrom( __DIR__.'/Lang', 'netgsm');
+
+        $this->publishes([
+            __DIR__.'/Config/netgsm.php' => config_path('netgsm.php'),
+        ], 'config');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Config
+        $this->mergeConfigFrom( __DIR__.'/Config/netgsm.php', 'netgsm');
+
+        $this->app['Netgsm'] = $this->app->share(function($app) {
+            return new Netgsm;
+        });
+    }
+}
